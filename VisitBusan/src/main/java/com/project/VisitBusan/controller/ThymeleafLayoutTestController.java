9ac1724dececa18c4ec34533903b7b1183f3966a
papepopopy <hapohee@naver.com>
2024-08-26 10:07:39 +0900

    보희 240826 thymeleaf 변환

--- VisitBusan/src/main/java/com/project/VisitBusan/controller/ThymeleafLayoutTestController.java
+++ VisitBusan/src/main/java/com/project/VisitBusan/controller/ThymeleafLayoutTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/layout")
 public class ThymeleafLayoutTestController {
     @GetMapping("base")
-    public String fragmenttest() {
+    public String base() {
         return "fragment/main";
     }
 }
