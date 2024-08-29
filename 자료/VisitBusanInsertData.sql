c55ca532d98dd2a8d33f626c30817ee1c37c4d8f
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-08-29 13:02:40 +0900

    태진 20240828 - 초기데이터설정파일 변경

--- 자료/VisitBusanInsertData.sql
+++ 자료/VisitBusanInsertData.sql
@@ -1,11 +1,17 @@
 -- Visit Busan
 -- 초기 데이터
 
+-- 데이터베이스 삭제
+-- DROP DATABASE visit_busan;
+
 -- 데이터베이스 생성
 -- CREATE DATABASE visit_busan;
 
 -- 데이터베이스 선택
 USE visit_busan;
+
+-- 선택된 데이터베이스의 테이블 목록 표시
+-- SHOW TABLES;
 
 -- ------------------------------
 --            멤버
@@ -15,14 +21,9 @@ INSERT INTO member
 (address, email, NAME, PASSWORD, user_id)
 VALUES
 ('root','root@test.com', 'root', '1234', 'root'),
-('admin1','admin@test.com', 'admin1', '1234', 'admin1'),
-('admin2','admin2@test.com', 'admin2', '1234', 'admin2'),
-('address1','test1@test.com', 'test1', '1234', 'testid1'),
-('address2','test2@test.com', 'test2', '1234', 'testid2'),
-('address3','test3@test.com', 'test3', '1234', 'testid3'),
-('address4','test4@test.com', 'test4', '1234', 'testid4'),
-('address5','test5@test.com', 'test5', '1234', 'testid5'),
-('address6','test6@test.com', 'test6', '1234', 'testid6');
+('admin','admin@test.com', 'admin', '1234', 'admin'),
+('user','user@test.com', 'user', '1234', 'user'),
+('test','test@test.com', 'test', '1234', 'test');
 SHOW COLUMNS FROM member;
 SELECT * FROM member;
 
@@ -40,13 +41,7 @@ VALUES
 (1,2),
 (1,1),
 (2,1),
-(3,1),
-(4,0),
-(5,0),
-(6,0),
-(7,0),
-(8,0),
-(9,0);
+(3,0);
 SHOW COLUMNS FROM member_role_set;
 SELECT * FROM member_role_set;
 
@@ -62,26 +57,26 @@ SELECT * FROM member_role_set;
 INSERT INTO board
 (category,title,content,writer,writer_id,tags,reg_date,mod_date)
 VALUES
-('Festival','title1','content1','writer1','writer_id1','good',SYSDATE(),SYSDATE()),
-('Festival','title2','content2','writer2','writer_id2','bad',SYSDATE(),SYSDATE()),
-('Performance','title3','content3','writer3','writer_id3','good',SYSDATE(),SYSDATE()),
-('Performance','title4','content4','writer4','writer_id4','bad',SYSDATE(),SYSDATE()),
-('Place','title5','content5','writer5','writer_id5','good',SYSDATE(),SYSDATE()),
-('Place','title6','content6','writer6','writer_id6','bad',SYSDATE(),SYSDATE()),
-('Food','title7','content7','writer7','writer_id7','good',SYSDATE(),SYSDATE()),
-('Food','title8','content8','writer8','writer_id8','bad',SYSDATE(),SYSDATE()),
-('Accommodation','title9','content9','writer9','writer_id9','good',SYSDATE(),SYSDATE()),
-('Accommodation','title10','content10','writer10','writer_id10','good',SYSDATE(),SYSDATE()),
-('Guide','title11','content11','writer11','writer_id11','bad',SYSDATE(),SYSDATE()),
-('Guide','title12','content12','writer12','writer_id12','good',SYSDATE(),SYSDATE()),
-('Preparation','title13','content13','writer13','writer_id13','bad',SYSDATE(),SYSDATE()),
-('Preparation','title14','content14','writer14','writer_id14','good',SYSDATE(),SYSDATE()),
-('Infomation','title15','content15','writer15','writer_id15','bad',SYSDATE(),SYSDATE()),
-('Infomation','title16','content16','writer16','writer_id16','good',SYSDATE(),SYSDATE()),
-('Schedule','title17','content17','writer17','writer_id17','good',SYSDATE(),SYSDATE()),
-('Schedule','title18','content18','writer18','writer_id18','good',SYSDATE(),SYSDATE()),
-('Review','title19','content19','writer19','writer_id19','good',SYSDATE(),SYSDATE()),
-('Review','title20','content20','writer20','writer_id20','good',SYSDATE(),SYSDATE());
+('Festival','title1','content1','admin','admin','good',SYSDATE(),SYSDATE()),
+('Festival','title2','content2','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Performance','title3','content3','admin','admin','good',SYSDATE(),SYSDATE()),
+('Performance','title4','content4','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Place','title5','content5','admin','admin','good',SYSDATE(),SYSDATE()),
+('Place','title6','content6','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Food','title7','content7','admin','admin','good',SYSDATE(),SYSDATE()),
+('Food','title8','content8','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Accommodation','title9','content9','admin','admin','good',SYSDATE(),SYSDATE()),
+('Accommodation','title10','content10','admin','admin','good',SYSDATE(),SYSDATE()),
+('Guide','title11','content11','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Guide','title12','content12','admin','admin','good',SYSDATE(),SYSDATE()),
+('Preparation','title13','content13','admin','admin','bad',SYSDATE(),SYSDATE()),
+('Preparation','title14','content14','admin','admin','good',SYSDATE(),SYSDATE()),
+('Infomation','title15','content15','user','user','bad',SYSDATE(),SYSDATE()),
+('Infomation','title16','content16','user','user','good',SYSDATE(),SYSDATE()),
+('Schedule','title17','content17','user','user','good',SYSDATE(),SYSDATE()),
+('Schedule','title18','content18','user','user','good',SYSDATE(),SYSDATE()),
+('Review','title19','content19','user','user','good',SYSDATE(),SYSDATE()),
+('Review','title20','content20','user','user','good',SYSDATE(),SYSDATE());
 SHOW COLUMNS FROM board;
 SELECT * FROM board;
 
@@ -93,15 +88,17 @@ SELECT * FROM board;
 INSERT INTO reply
 (board_id,reply_text,replier,replier_id,reg_date,mod_date)
 VALUES
-(1,'reply_text1','test1','testid1',SYSDATE(),SYSDATE()),
-(3,'reply_text2','test2','testid2',SYSDATE(),SYSDATE()),
-(5,'reply_text3','test3','testid3',SYSDATE(),SYSDATE()),
-(7,'reply_text4','test4','testid4',SYSDATE(),SYSDATE()),
-(9,'reply_text5','test5','testid5',SYSDATE(),SYSDATE()),
-(11,'reply_text6','test6','testid6',SYSDATE(),SYSDATE()),
-(13,'reply_text7','test7','testid7',SYSDATE(),SYSDATE()),
-(15,'reply_text8','test8','testid8',SYSDATE(),SYSDATE()),
-(17,'reply_text9','test9','testid9',SYSDATE(),SYSDATE()),
-(19,'reply_text10','test10','testid10',SYSDATE(),SYSDATE());
+(1,'reply_text1','user','user',SYSDATE(),SYSDATE()),
+(3,'reply_text2','user','user',SYSDATE(),SYSDATE()),
+(5,'reply_text3','user','user',SYSDATE(),SYSDATE()),
+(7,'reply_text4','user','user',SYSDATE(),SYSDATE()),
+(9,'reply_text5','user','user',SYSDATE(),SYSDATE()),
+(11,'reply_text6','user','user',SYSDATE(),SYSDATE()),
+(13,'reply_text7','user','user',SYSDATE(),SYSDATE()),
+(15,'reply_text8','user','user',SYSDATE(),SYSDATE()),
+(17,'reply_text9','user','user',SYSDATE(),SYSDATE()),
+(19,'reply_text10','user','user',SYSDATE(),SYSDATE());
 SHOW COLUMNS FROM reply;
 SELECT * FROM reply;
+
+
