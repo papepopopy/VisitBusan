a5f4393e2f63cec6b2a51cc83b2a8b52dd8b32ce
papepopoy <hapohee@naver.com>
2024-09-12 15:48:03 +0900

    태진 240912 회원정보 수정 유효성 검사 수정(2)

--- VisitBusan/src/main/java/com/project/VisitBusan/controller/MemberController.java
+++ VisitBusan/src/main/java/com/project/VisitBusan/controller/MemberController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.spi.ErrorMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -155,31 +157,41 @@ public class MemberController {
 
 
     @PostMapping(value = "/mypage/modify/check") //데이터 전송
-    public String updateMemberCheck(@Valid @ModelAttribute MemberDTO memberDTO, //valid 유효성 검사
-                                    BindingResult bindingResult){ //유효성 검사 후 데이터 담는 객체
+    public ResponseEntity<String> updateMemberCheck(@Valid @ModelAttribute MemberDTO memberDTO,
+                                                    BindingResult bindingResult){
 
         log.info("updateMemberCheck ================>"+memberDTO);
 
-
         String errorMessage = "";
+        String password = memberDTO.getPassword();
 
         // 유효성 검사 오류 처리
-        if (bindingResult.hasErrors()) {
+        if (password.isEmpty()) {
             log.info("test2 ================>");
             errorMessage ="비밀번호를 입력해주세요.";
-        }
+
+        } else {
 
         // 멤버 조회
         MemberDTO member = memberService.findMember(memberDTO.getUserId());
 
-        // 비밀번호 확인
-        if(!passwordEncoder.matches(memberDTO.getPassword(), member.getPassword())) {
-            log.info("test3 ================>");
-            errorMessage ="비밀번호가 일치하지 않습니다.";
+            // 비밀번호 확인
+            if(!passwordEncoder.matches(memberDTO.getPassword(), member.getPassword())) {
+                log.info("test3 ================>");
+                errorMessage ="비밀번호가 일치하지 않습니다.";
+            } else if (bindingResult.hasErrors()) {
+                log.info("test2 ================>");
+
+                errorMessage = bindingResult.toString();
+
+            }
+
         }
+
         log.info("errorMessage ================>"+errorMessage);
 
-        return errorMessage;
+        return ResponseEntity.ok().body(errorMessage);
+
     }
 
 
@@ -190,8 +202,8 @@ public class MemberController {
     public String updateMember(@Valid @ModelAttribute MemberDTO memberDTO, //valid 유효성 검사
                                BindingResult bindingResult, //유효성 검사 후 데이터 담는 객체
                                RedirectAttributes redirectAttributes) { //일회성 데이터 전달
-        log.info("test ================>");
-        String showswich = "1";
+
+        //String showswich = "1";
 
         // 유효성 검사 오류 처리
         if (bindingResult.hasErrors()) {
@@ -199,10 +211,11 @@ public class MemberController {
 
             // 1회용 정보유지 : redirect방식으로 요청시 정보관리하는 객체
             redirectAttributes.addFlashAttribute("errorMessage", "비밀번호를 입력해주세요.");
-            redirectAttributes.addFlashAttribute("showswich", showswich);
+            //redirectAttributes.addFlashAttribute("showswich", showswich);
             return "redirect:/mypage"; // 유효성 검사 오류 시 다시 마이페이지로 이동
 
         }
+        /*
         // 멤버 조회
         MemberDTO member = memberService.findMember(memberDTO.getUserId());
 
@@ -217,6 +230,7 @@ public class MemberController {
             return "redirect:/mypage";
         }
         log.info("test4 ================>");
+        */
 
         //회원정보 수정
         memberService.modify(memberDTO);
