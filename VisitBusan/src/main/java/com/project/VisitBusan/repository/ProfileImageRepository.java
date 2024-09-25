dbbbf68a09bbae1d771a779ff7626c0ca8e8cc52
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-09-25 17:09:53 +0900

    태진 20240925 - 축제페이지 수정

--- VisitBusan/src/main/java/com/project/VisitBusan/repository/ProfileImageRepository.java
+++ VisitBusan/src/main/java/com/project/VisitBusan/repository/ProfileImageRepository.java
@@ -2,7 +2,7 @@ package com.project.VisitBusan.repository;
 
 import com.project.VisitBusan.entity.Member;
 import com.project.VisitBusan.entity.ProfileImage;
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
+//import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import lombok.Builder;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
